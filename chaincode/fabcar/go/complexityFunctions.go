20d1088b745a8b5a4cecfcb3bb3fb78aacf27725
Muhammad Haroon Arshad <haroon_ars@hotmail.com>
2020-01-16 00:44:37 +0100

    - Added Golang files for dummyData and complexity functions

---
+++ chaincode/fabcar/go/complexityFunctions.go
@@ -0,0 +1,17 @@
+/*
+ * Complexity functions
+ */
+
+package complexityFunction
+
+/* Imports
+ * 4 utility libraries for formatting, handling bytes, reading and writing JSON, and string manipulation
+ * 2 specific Hyperledger Fabric specific libraries for Smart Contracts
+ */
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	dummyData "./dummyData"
+)
