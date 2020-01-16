1e793de9ed8313565586ead0f0dbf49d50d77a00
Muhammad Haroon Arshad <haroon_ars@hotmail.com>
2020-01-16 11:54:48 +0100

    - checkstyles fixed for java

--- chaincode/fabcar/java/src/main/java/org/hyperledger/fabric/samples/fabcar/ComplexityFunctions.java
+++ chaincode/fabcar/java/src/main/java/org/hyperledger/fabric/samples/fabcar/ComplexityFunctions.java
@@ -53,8 +53,9 @@ public final class ComplexityFunctions {
         int[] list = this.dummyData.getNumbers15();
         int[][] matrix = new int[15][15];
         for (int i = 0, total = list.length; i < total; i++) {
-            for (int j = 0, total2 = list.length; j < total2; j++)
+            for (int j = 0, total2 = list.length; j < total2; j++) {
                 matrix[i][j] = list[j];
+            }
         }
         return matrix;
     };
@@ -62,7 +63,7 @@ public final class ComplexityFunctions {
     /**
      * complexity O(log(n))
      */
-    public int binarySearch(int arr[], int x) {
+    public int binarySearch(final int[] arr, final int x) {
         // to test (sorted)
         // int arr[] =
         // {31,695,708,802,894,1326,1616,1927,2278,3821,4290,4681,5746,7828,9130};
@@ -70,21 +71,25 @@ public final class ComplexityFunctions {
         // int x = 4681;
         // int result = binarySearch(arr, x);
         // System.out.println("got: " + result);
-        int l = 0, r = arr.length - 1;
+        int l = 0;
+        int r = arr.length - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
 
             // Check if x is present at mid
-            if (arr[m] == x)
-                return m;
+            if (arr[m] == x) {
+                return m; 
+            }
 
             // If x greater, ignore left half
-            if (arr[m] < x)
-                l = m + 1;
+            if (arr[m] < x) {
+                l = m + 1; 
+            }
 
             // If x is smaller, ignore right half
-            else
-                r = m - 1;
+            else {
+                r = m - 1; 
+            }
         }
         return -1;
     }
@@ -92,34 +97,37 @@ public final class ComplexityFunctions {
     /**
      * complexity O(nlog(n))
      */
-    public void merge(int arr[], int l, int m, int r) {
+    public void merge(final int[] arr, final int l, final int m, final int r) {
         // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
         /* Create temp arrays */
-        int L[] = new int[n1];
-        int R[] = new int[n2];
+        int[] arrLeft = new int[n1];
+        int[] arrRight = new int[n2];
 
         /* Copy data to temp arrays */
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[m + 1 + j];
+        for (int i = 0; i < n1; ++i) {
+            arrLeft[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            arrRight[j] = arr[m + 1 + j];
+        }
 
         /* Merge the temp arrays */
 
         // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
 
         // Initial index of merged subarry array
         int k = l;
         while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
+            if (arrLeft[i] <= arrRight[j]) {
+                arr[k] = arrLeft[i];
                 i++;
             } else {
-                arr[k] = R[j];
+                arr[k] = arrRight[j];
                 j++;
             }
             k++;
@@ -127,21 +135,21 @@ public final class ComplexityFunctions {
 
         /* Copy remaining elements of L[] if any */
         while (i < n1) {
-            arr[k] = L[i];
+            arr[k] = arrLeft[i];
             i++;
             k++;
         }
 
         /* Copy remaining elements of R[] if any */
         while (j < n2) {
-            arr[k] = R[j];
+            arr[k] = arrRight[j];
             j++;
             k++;
         }
     }
 
     // Main function that sorts arr[l..r] using merge()
-    public void sort(int[] arr, int l, int r) {
+    public void sort(final int[] arr, final int l, final int r) {
         // to test
         // int[] arr = {802, 894, 4681, 2278, 695, 5746, 9130, 1326, 708, 1927, 31,
         // 4290, 1616, 7828, 3821};
@@ -166,17 +174,18 @@ public final class ComplexityFunctions {
     /**
      * Helper function to print array
      */
-    public void printArray(int arr[]) {
+    public void printArray(final int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; ++i) {
             System.out.print(arr[i] + " ");
+        }
         System.out.println();
     }
 
     /**
      * complexity O(2^n)
      */
-    public void powerset(String input) {
+    public void powerset(final String input) {
         // For test
         // powerset("abcdef");
         for (int i = 1; i < (int) Math.pow(2, input.length()); i++) {
@@ -200,7 +209,7 @@ public final class ComplexityFunctions {
     /**
      * complexity O(n!)
      */
-    public void getPermutation(String prefix, String str) {
+    public void getPermutation(final String prefix, final String str) {
         // For test
         // permutation("", str);
 
